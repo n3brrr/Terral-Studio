397325c1619b012d2a15819ace45b76d20b1a6d1
Ruben <neburtl837@gmail.com>
2026-07-20 18:38:55 +0200

    refactor layout grid

--- src/components/ui/layout-grid.tsx
+++ src/components/ui/layout-grid.tsx
@@ -1,6 +1,4 @@
 "use client";
-import React from "react";
-import { motion } from "framer-motion";
 import { cn } from "../../lib/utils";
 
 export const LayoutGrid = ({ cards }: { cards: any[] }) => {
@@ -8,18 +6,13 @@ export const LayoutGrid = ({ cards }: { cards: any[] }) => {
     <div className="w-full h-full p-6 md:p-10 grid grid-cols-1 md:grid-cols-3 max-w-7xl mx-auto gap-4">
       {cards.map((card, i) => (
         <div key={i} className={cn(card.className, "relative overflow-hidden")}>
-          <motion.div
-            className={cn(
-              "cursor-pointer overflow-hidden relative h-full w-full",
-            )}
-          >
-            {/* IMAGEN */}
+          <div className="overflow-hidden relative h-full w-full">
             <img
               src={card.thumbnail}
               alt="thumbnail"
               className="object-cover w-full h-full rounded-xl aspect-video md:aspect-auto"
             />
-          </motion.div>
+          </div>
         </div>
       ))}
     </div>
